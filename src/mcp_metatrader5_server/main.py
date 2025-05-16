fc4d8a5079d2511c1d07f099f238a6ce4f2c11b5
chowderhead <nestedcallbacks@gmail.com>
2025-05-17 02:36:22 +0300

    update

--- src/mcp_metatrader5_server/main.py
+++ src/mcp_metatrader5_server/main.py
@@ -451,4 +451,4 @@ tick = get_symbol_info_tick("EURUSD")
 """
 
 # Create a proper ASGI app from the FastMCP object
-app = mcp.streamable_http_app(path='/sse/')
+app = mcp.streamable_http_app(path='/sse', include_in_schema=True, redirect_slashes=False)
